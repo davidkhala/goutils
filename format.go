7fe30d1d79179fae46bc45bb9d6f232d3642558b
davidliu <david-khala@hotmail.com>
2018-09-19 12:48:15 +0800

    Add RandString

--- format.go
+++ format.go
@@ -1,9 +1,10 @@
 package goutils
 
 import (
-	"strconv"
 	"encoding/json"
 	"errors"
+	"math/rand"
+	"strconv"
 	"time"
 )
 
@@ -68,4 +69,29 @@ func ToJson(v interface{}) []byte {
 	result, err := json.Marshal(v)
 	PanicError(err)
 	return result
+}
+
+//not thread safe
+func RandString(length int) string {
+	var src = rand.NewSource(time.Now().UnixNano())
+
+	var letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	var letterIdxBits = uint(6)                     // 6 bits to represent a letter index
+	var letterIdxMask = int64(1<<letterIdxBits - 1) // All 1-bits, as many as letterIdxBits
+	var letterIdxMax = 63 / letterIdxBits           // # of letter indices fitting in 63 bits
+	b := make([]byte, length)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := length-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
 }
