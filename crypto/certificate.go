d9a07c31d2c700094e5dd04b5f4afcabf4f0049c
davidliu <david-khala@hotmail.com>
2018-12-25 18:02:54 +0800

    copy master cid GetDN

--- crypto/certificate.go
+++ crypto/certificate.go
@@ -2,7 +2,11 @@ package crypto
 
 import (
 	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/asn1"
+	"encoding/hex"
 	"encoding/pem"
+	"fmt"
 	. "github.com/davidkhala/goutils"
 )
 
@@ -12,4 +16,67 @@ func ParseCertPem(pemBytes []byte) *x509.Certificate {
 	cert, err := x509.ParseCertificate(block.Bytes)
 	PanicError(err)
 	return cert
+}
+
+// Get the DN (distinguished name) associated with a pkix.Name.
+// NOTE: This code is almost a direct copy of the String() function in
+// https://go-review.googlesource.com/c/go/+/67270/1/src/crypto/x509/pkix/pkix.go#26
+// which returns a DN as defined by RFC 2253.
+func GetDN(name *pkix.Name) string {
+	r := name.ToRDNSequence()
+	s := ""
+	for i := 0; i < len(r); i++ {
+		rdn := r[len(r)-1-i]
+		if i > 0 {
+			s += ","
+		}
+		for j, tv := range rdn {
+			if j > 0 {
+				s += "+"
+			}
+			typeString := tv.Type.String()
+			typeName, ok := AttributeTypeNames[typeString]
+			if !ok {
+				derBytes, err := asn1.Marshal(tv.Value)
+				if err == nil {
+					s += typeString + "=#" + hex.EncodeToString(derBytes)
+					continue // No value escaping necessary.
+				}
+				typeName = typeString
+			}
+			valueString := fmt.Sprint(tv.Value)
+			escaped := ""
+			begin := 0
+			for idx, c := range valueString {
+				if (idx == 0 && (c == ' ' || c == '#')) ||
+					(idx == len(valueString)-1 && c == ' ') {
+					escaped += valueString[begin:idx]
+					escaped += "\\" + string(c)
+					begin = idx + 1
+					continue
+				}
+				switch c {
+				case ',', '+', '"', '\\', '<', '>', ';':
+					escaped += valueString[begin:idx]
+					escaped += "\\" + string(c)
+					begin = idx + 1
+				}
+			}
+			escaped += valueString[begin:]
+			s += typeName + "=" + escaped
+		}
+	}
+	return s
+}
+
+var AttributeTypeNames = map[string]string{
+	"2.5.4.6":  "C",
+	"2.5.4.10": "O",
+	"2.5.4.11": "OU",
+	"2.5.4.3":  "CN",
+	"2.5.4.5":  "SERIALNUMBER",
+	"2.5.4.7":  "L",
+	"2.5.4.8":  "ST",
+	"2.5.4.9":  "STREET",
+	"2.5.4.17": "POSTALCODE",
 }
