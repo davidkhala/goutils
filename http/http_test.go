aece9bee0f89c0a64d3239fc2e7f075cdba90f68
davidLiu <david-khala@hotmail.com>
2019-01-15 14:02:01 +0800

    test timeout

--- http/http_test.go
+++ http/http_test.go
@@ -9,4 +9,8 @@ func TestGet(t *testing.T) {
 	resp := Get("http://www.google.com", nil)
 	fmt.Println(string(resp.BodyBytes()))
 }
+func TestTimeout(t *testing.T) {
+	resp := Get("http://abc.google.com", nil)
+	fmt.Println(string(resp.BodyBytes()))
+}
 
