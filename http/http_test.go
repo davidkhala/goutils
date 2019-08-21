36bec69bcf5534e93b338cfef47efa508f9731c6
david <david-khala@hotmail.com>
2019-08-21 10:16:02 +0800

    add Regexp

--- http/http_test.go
+++ http/http_test.go
@@ -14,7 +14,7 @@ func TestGet(t *testing.T) {
 func TestTimeout(t *testing.T) {
 	var invalidUrl = "http://abc.google.com"
 	var handler = func(errString string, params ...interface{}) (success bool) {
-		return assert.Equal(t, "Get "+invalidUrl+": dial tcp: lookup abc.google.com: no such host", errString)
+		return assert.Regexp(t, "^Get "+invalidUrl+": dial tcp: lookup ", errString)
 	}
 	defer goutils.Deferred(handler)
 	Get(invalidUrl, nil)
