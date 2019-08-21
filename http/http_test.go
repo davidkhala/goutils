147febf4b41b2ac54b9bc56854e896eae97ec5e8
david <david-khala@hotmail.com>
2019-08-21 10:00:50 +0800

    add http error assert

--- http/http_test.go
+++ http/http_test.go
@@ -2,6 +2,8 @@ package http
 
 import (
 	"fmt"
+	"github.com/davidkhala/goutils"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -10,7 +12,11 @@ func TestGet(t *testing.T) {
 	fmt.Println(string(resp.BodyBytes()))
 }
 func TestTimeout(t *testing.T) {
-	resp := Get("http://abc.google.com", nil)
-	fmt.Println(string(resp.BodyBytes()))
-}
+	var invalidUrl = "http://abc.google.com"
+	var handler = func(errString string, params ...interface{}) (success bool) {
+		return assert.Equal(t, "Get "+invalidUrl+": dial tcp: lookup abc.google.com: no such host", errString)
+	}
+	defer goutils.Deferred(handler)
+	Get(invalidUrl, nil)
 
+}
