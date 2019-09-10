28480c306db0b24cc6b71d6e8901c92190f5470b
davidLiu <david-khala@hotmail.com>
2019-09-10 10:20:10 +0800

    fix scripts test

--- scripts/test/goCMDTest.sh
+++ scripts/test/goCMDTest.sh
@@ -6,6 +6,8 @@ for ((i = 2; i <= ${#}; i++)); do
     j=${!i}
     remain_params="$remain_params $j"
 done
-../goCmd.sh getAndEnsure github.com/MediConCenHK/go-chaincode-common
-../goCmd.sh getAndEnsure https://github.com/MediConCenHK/go-chaincode-common.git
-../goCmd.sh getAndEnsure git@github.com:MediConCenHK/go-chaincode-common.git
+CURRENT=$(cd $(dirname ${BASH_SOURCE}) && pwd)
+root=$(dirname $CURRENT)
+$root/goCmd.sh getAndEnsure github.com/MediConCenHK/go-chaincode-common
+$root/goCmd.sh getAndEnsure https://github.com/MediConCenHK/go-chaincode-common.git
+$root/goCmd.sh getAndEnsure git@github.com:MediConCenHK/go-chaincode-common.git
