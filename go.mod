3ef0b961b87bc77012bb3e9046876dec2d03eb14
davidLiu <david-khala@hotmail.com>
2019-11-25 13:10:56 +0800

    [TODO] go: gopkg.in/go-playground/validator.v9@v9.30.1: go.mod has non-....v9 module path "github.com/go-playground/validator/v10" at revision v9.30.1

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/davidkhala/goutils
+
+require (
+	github.com/gin-gonic/gin v1.5.0
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/golang/protobuf v1.3.2
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/json-iterator/go v1.1.8 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/pkg/errors v0.8.1
+	github.com/stretchr/testify v1.4.0
+	github.com/syndtr/goleveldb v1.0.0
+	golang.org/x/sys v0.0.0-20191120155948-bd437916bb0e // indirect
+	gopkg.in/yaml.v2 v2.2.7 // indirect
+)
--- scripts/goCmd.sh
+++ scripts/goCmd.sh
@@ -3,52 +3,63 @@ set -e
 fcn=$1
 remain_params=""
 for ((i = 2; i <= ${#}; i++)); do
-    j=${!i}
-    remain_params="$remain_params $j"
+	j=${!i}
+	remain_params="$remain_params $j"
 done
 
 get() {
-    # support github and linux/unix only
-    local reposURL=$1
-    local orgName
-    local projectName
-    local GOPATH=$(go env GOPATH)
+	# support github and linux/unix only
+	local reposURL=$1
+	local orgName
+	local projectName
+	local GOPATH=$(go env GOPATH)
 
-    if [[ ${reposURL} == github* ]]; then
-        echo ...using native go get format
-        export GIT_TERMINAL_PROMPT=1
-        go get -u -v $reposURL
-        echo ${GOPATH}/src/$reposURL
-    elif [[ ${reposURL} == https://* ]]; then
-        orgName=$(echo ${reposURL} | cut -d '/' -f 4)
-        projectName=$(echo ${reposURL} | cut -d '/' -f 5 | cut -d '.' -f 1)
-        get github.com/${orgName}/$projectName # recursive
-    elif [[ ${reposURL} == git@* ]]; then
-        echo ...using SSH
-        orgName=$(echo ${reposURL} | cut -d '/' -f 1 | cut -d ':' -f 2)
-        projectName=$(echo ${reposURL} | cut -d '/' -f 2 | cut -d '.' -f 1)
+	if [[ ${reposURL} == github* ]]; then
+		echo ...using native go get format
+		export GIT_TERMINAL_PROMPT=1
+		go get -u -v $reposURL
+		echo ${GOPATH}/src/$reposURL
+	elif [[ ${reposURL} == https://* ]]; then
+		orgName=$(echo ${reposURL} | cut -d '/' -f 4)
+		projectName=$(echo ${reposURL} | cut -d '/' -f 5 | cut -d '.' -f 1)
+		get github.com/${orgName}/$projectName # recursive
+	elif [[ ${reposURL} == git@* ]]; then
+		echo ...using SSH
+		orgName=$(echo ${reposURL} | cut -d '/' -f 1 | cut -d ':' -f 2)
+		projectName=$(echo ${reposURL} | cut -d '/' -f 2 | cut -d '.' -f 1)
 
-        local orgPath=${GOPATH}/src/github.com/${orgName}
-        mkdir -p ${orgPath}
-        cd ${orgPath}
-        if [[ ! -d ${orgPath}/${projectName} ]]; then
-            git clone $1
-        else
-            cd ${orgPath}/${projectName}
-            git pull
-        fi
-        echo ${orgPath}/${projectName}
-    else
-        exit 1
-    fi
+		local orgPath=${GOPATH}/src/github.com/${orgName}
+		mkdir -p ${orgPath}
+		cd ${orgPath}
+		if [[ ! -d ${orgPath}/${projectName} ]]; then
+			git clone $1
+		else
+			cd ${orgPath}/${projectName}
+			git pull
+		fi
+		echo ${orgPath}/${projectName}
+	else
+		exit 1
+	fi
 }
 
 getAndEnsure() {
-    local projectPath
-    projectPath=$(get "$1" | tail -1)
-    cd "${projectPath}"
-    dep ensure
-    cd - >/dev/null
+	local projectPath
+	projectPath=$(get "$1" | tail -1)
+	cd "${projectPath}"
+	dep ensure
+	cd - >/dev/null
+}
+
+setModuleMode() {
+	modes=('off' 'on' 'auto')
+	if [[ " ${modes[*]} " == *"$1"* ]]; then
+		export GO111MODULE=$1
+		echo "export GO111MODULE=$1"
+	else
+		echo "illegal GO111MODULE [${1}]"
+		exit 1
+	fi
 }
 
 $fcn $remain_params
