e4dafa8f3048c2700980fa072584a88fd3a1bef0
david <david-khala@hotmail.com>
2019-11-25 22:09:53 +0800

    fix for go mod

--- scripts/goCmd.sh
+++ scripts/goCmd.sh
@@ -17,12 +17,21 @@ get() {
 	if [[ ${reposURL} == github* ]]; then
 		echo ...using native go get format
 		export GIT_TERMINAL_PROMPT=1
-		go get -u -v $reposURL
+		go get -u -v $reposURL # TODO not good in go mod
 		echo ${GOPATH}/src/$reposURL
 	elif [[ ${reposURL} == https://* ]]; then
 		orgName=$(echo ${reposURL} | cut -d '/' -f 4)
 		projectName=$(echo ${reposURL} | cut -d '/' -f 5 | cut -d '.' -f 1)
-		get github.com/${orgName}/$projectName # recursive
+		local orgPath=${GOPATH}/src/github.com/${orgName}
+		mkdir -p ${orgPath}
+		cd ${orgPath}
+		if [[ ! -d ${orgPath}/${projectName} ]]; then
+			git clone $1
+		else
+			cd ${orgPath}/${projectName}
+			git pull
+		fi
+		echo ${orgPath}/${projectName}
 	elif [[ ${reposURL} == git@* ]]; then
 		echo ...using SSH
 		orgName=$(echo ${reposURL} | cut -d '/' -f 1 | cut -d ':' -f 2)
