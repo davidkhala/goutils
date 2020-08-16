21504f6940122e7d873abcaa84e5af6686d0aa4f
Yuxiang LIU <david-khala@hotmail.com>
2020-08-16 14:38:38 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 set -e
-fcn=$1
-remain_params=""
-for ((i = 2; i <= ${#}; i++)); do
-	j=${!i}
-	remain_params="$remain_params $j"
-done
 
 isMacOS() {
 	[[ $(uname) == "Darwin" ]]
@@ -40,4 +34,4 @@ latest() {
 		fi
 	fi
 }
-$fcn $remain_params
+"$@"
